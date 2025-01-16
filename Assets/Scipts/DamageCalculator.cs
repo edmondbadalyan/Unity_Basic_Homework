88f24ed70835d30fba82bf693c845fcc35d760cc
Edmond.bad <edmondbadalyan@gmail.com>
2025-01-16 15:51:30 +0300

    Внесены правки в стиль кода

---
+++ Assets/Scipts/DamageCalculator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scipts
+{
+    public class DamageCalculator
+    {
+        public void CalculateDamage(float baseDamage, int damageMultiplier)
+        {
+            var result = baseDamage * damageMultiplier;
+            Debug.LogWarning($"Calculated Damage: {baseDamage} * {damageMultiplier} = {result}");
+        }
+    }
+}
--- Assets/Scipts/Test.cs
+++ Assets/Scipts/Test.cs
@@ -1,38 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+using Assets.Scipts;
 using UnityEngine;
 
-public class DamageCalculator
-{
-    public void CalculateDamage(float baseDamage, int multiplier)
-    {
-        var result = baseDamage * multiplier;
-        Debug.LogWarning($"Calculated Damage: {baseDamage} * {multiplier} = {result}");
-    }
-}
 public class Test : MonoBehaviour
 {
+    public int Multiplier = 10;
+    public float BaseDamage = 1.0f;
+    public bool IsDamaged = false;
+    private DamageCalculator _damageCalculator;
 
-    public int multiplier = 10;
-    public float baseDamage = 1.0f;
-    public bool isDamaged = false;
-    public DamageCalculator damageCalculator;
-
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        Debug.Log($"Base Damage (float): {baseDamage}");
-        Debug.Log($"Multiplier (int): {multiplier}");
-        Debug.Log($"Is Damaged (bool): {isDamaged}");
+        Debug.Log($"Base Damage (float): {BaseDamage}");
+        Debug.Log($"Multiplier (int): {Multiplier}");
+        Debug.Log($"Is Damaged (bool): {IsDamaged}");
 
-        damageCalculator = new DamageCalculator();
-        damageCalculator.CalculateDamage(baseDamage, multiplier);
+        _damageCalculator = new DamageCalculator();
+        _damageCalculator.CalculateDamage(BaseDamage, Multiplier);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 }
